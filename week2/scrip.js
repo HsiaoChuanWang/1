3c408ed735bac330b5faf31c7d0f0b1121f97121
HsiaoChuanWang <j2130970@gmail.com>
2023-09-10 16:09:06 +0800

    add JS

---
+++ week2/scrip.js
@@ -0,0 +1,37 @@
+//Request 1:
+const clickwelcome=document.getElementsByTagName('h2');
+
+clickwelcome[0].addEventListener('click',(event)=>{
+    event.target.textContent="Have a Good Time!";
+})
+
+//Request 2:
+const icons=document.querySelector('.icons');
+const closeButton=document.querySelector('.closebutton');
+const showList=document.querySelector('.showlist');
+
+icons.addEventListener('click',()=>{
+    if(showList.style.display==='none'){
+        showList.style.display='block';
+    }
+    else{
+        showList.style.display='none';
+    }
+})
+
+closeButton.addEventListener('click',()=>{
+    if(showList.style.display==='block'){
+        showList.style.display='none';
+    }
+    else{
+        showList.style.display='block';
+    }
+})
+
+//Request 3:
+const clickAction=document.querySelector('.call');
+const boxes2=document.querySelector('.boxes2');
+
+clickAction.addEventListener('click',()=>{
+    boxes2.style.display="flex";
+})
